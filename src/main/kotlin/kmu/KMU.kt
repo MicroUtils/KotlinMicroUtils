1294e787fb50da442ac72e1201b02aafaa42dbd2
oshai <ohadshai@gmail.com>
2016-08-15 23:58:55 +0300

    added printDebug

--- src/main/kotlin/kmu/KMU.kt
+++ src/main/kotlin/kmu/KMU.kt
@@ -4,3 +4,5 @@ package kmu
 inline fun <T> T.match(predicate: (T) -> Boolean) = predicate(this)
 
 inline fun <T> XXX(): T = throw NotImplementedError()
+
+fun <T> T.printDebug() : T = this.apply { println(this) }
